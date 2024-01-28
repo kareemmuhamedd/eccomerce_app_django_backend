96693f3eee1be473579fd40347fd1e291c51d5c6
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-01-28 22:51:10 +0200

    solving errors

--- core/backend/models.py
+++ core/backend/models.py
@@ -1,12 +1,15 @@
+from random import choice
+import re
+import uuid
 from django.db import models
-from django.utils import timezone
 
 
+# Create your models here.
 class User(models.Model):
     email = models.EmailField()
-    phone = models.CharField(max_length=16)
-    fullname = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+    fullname = models.CharField(max_length=100)
+    password = models.CharField(max_length=5000)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -14,7 +17,7 @@ class User(models.Model):
 
 
 class Otp(models.Model):
-    phone = models.CharField(max_length=16)
+    phone = models.CharField(max_length=10)
     otp = models.IntegerField()
     validity = models.DateTimeField()
     verified = models.BooleanField(default=False)
@@ -24,9 +27,8 @@ class Otp(models.Model):
 
 
 class Token(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tokens_set")
     token = models.CharField(max_length=5000)
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='tokens_set')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -34,9 +36,97 @@ class Token(models.Model):
 
 
 class PasswordResetToken(models.Model):
-    token = models.CharField(max_length=5000)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='password_reset_tokens_set')
-    validity=timezone.now() + timezone.timedelta(days=1)
+        User, on_delete=models.CASCADE, related_name="password_reset_tokens_set"
+    )
+    token = models.CharField(max_length=5000)
+    created_at = models.DateTimeField()
+
     def __str__(self):
         return self.user.email
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    position = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="categories/")
+
+    def __str__(self):
+        return self.name
+
+
+class Slide(models.Model):
+    position = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="slides/")
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products_set"
+    )
+    description = models.TextField()
+    price = models.FloatField()
+    offer_price = models.FloatField(default=0)
+    delivery_charge = models.FloatField(default=0)
+    star_5 = models.IntegerField(default=0)
+    star_4 = models.IntegerField(default=0)
+    star_3 = models.IntegerField(default=0)
+    star_2 = models.IntegerField(default=0)
+    star_1 = models.IntegerField(default=0)
+    cod = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductOption(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="options_set",
+    )
+    option = models.CharField(max_length=50)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.product.title} - {self.option}"
+
+
+class ProductImage(models.Model):
+    position = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="products/")
+    product_option = models.ForeignKey(
+        ProductOption,
+        on_delete=models.CASCADE,
+        related_name="images_set",
+    )
+
+
+class PageItem(models.Model):
+    position = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="pages/", blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="pageitems_set",
+    )
+    choices = [
+        (1, "BANNER"),
+        (2, "SWIPER"),
+        (3, "GRID"),
+    ]
+    viewtype = models.IntegerField(choices=choices)
+    title = models.CharField(max_length=50, blank=True)
+    product_options = models.ManyToManyField(ProductOption, blank=True)
+
+    def __str__(self):
+        return f"{self.category.name} - {self.title}"
--- core/backend/utils.py
+++ core/backend/utils.py
@@ -1,95 +1,80 @@
+from datetime import timedelta, timezone
 import datetime
-from django.core.mail import EmailMessage
 from random import randint
 import uuid
 from rest_framework.response import Response
 
 from backend.models import Otp, PasswordResetToken, Token
 from core.settings import TEMPLATES_BASE_URL
+from django.core.mail import EmailMessage
 from django.template.loader import get_template
+from rest_framework.permissions import BasePermission
 
 
 def send_otp(phone):
-
+    # todo sms api
     otp = randint(100000, 999999)
     validity = datetime.datetime.now() + datetime.timedelta(minutes=10)
-    Otp.objects.update_or_create(phone=phone, defaults={
-                                 "otp": otp, "verified": False, "validity": validity, })
+    Otp.objects.update_or_create(
+        phone=phone,
+        defaults={
+            "otp": otp,
+            "validity": validity,
+            "verified": False,
+        },
+    )
     print(otp)
-    # todo sms api
-    return Response('send otp successfully')
+    return Response("otp sent successfully")
 
 
 def new_token():
-    token = uuid.uuid1().hex
+    token = uuid.uuid4().hex
     return token
 
 
-def token_reponse(user):
+def token_response(user):
     token = new_token()
-    Token.objects.create(token=token, user=user)
+    Token.objects.create(user=user, token=token)
+    return Response({"token": token})
 
-    return Response('token'+token)
 
 def send_password_reset_email(user):
+    token = new_token()
+    exp_time = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    PasswordResetToken.objects.update_or_create(
+        user=user,
+        defaults={
+            "user": user,
+            "token": token,
+            "created_at": exp_time,
+        },
+    )
+    email_data = {
+        "token": token,
+        "email": user.email,
+        "base_url": TEMPLATES_BASE_URL,
+    }
+
+    message = get_template("emails/reset-password.html").render(email_data)
+
+    msg = EmailMessage(
+        "Reset Password",
+        body=message,
+        to=[user.email],
+    )
+
+    msg.content_subtype = "html"
+
     try:
-        # Generate a new token and expiration time
-        token = new_token()
-        exp_time = datetime.datetime.now() + datetime.timedelta(minutes=10)
-
-        # Create or update the PasswordResetToken object for the user
-        token_obj, created = PasswordResetToken.objects.update_or_create(
-            user=user,
-            defaults={'token': token, 'expiration_time': exp_time}
-        )
-
-        # Prepare data for the email template
-        email_data = {
-            'token': token,
-            'email': user.email,
-            'base_url': TEMPLATES_BASE_URL,
-        }
-
-        # Render the email template
-        message = get_template('emails/reset_password.html').render(email_data)
-
-        # Create and send the email
-        msg = EmailMessage('Reset Password', message, to=[user.email])
-        msg.content_subtype = 'html'
         msg.send()
-
-        return Response('reset_password_email_sent')
     except Exception as e:
-        print(f'An error occurred: {str(e)}')
-        # You may want to log the error or handle it in a more appropriate way.
-        return Response('An error occurred while sending the password reset email.')
+        print(e)
+        return Response("unable to send password reset email", status=400)
+    return Response("reset_password_email_sent")
 
 
-def send_password_reset_email(user):
-    try:
-        token = new_token()
-        exp_time = datetime.datetime.now() + datetime.timedelta(minutes=10)
+class IsAuthenticatedUser(BasePermission):
+    message = "unauthenticated_user"
 
-        PasswordResetToken.objects.update_or_create(
-            user=user, defaults={'user': user, 'token': token, })
-        email_data = {
-            'token': token,
-            'email': user.email,
-            'base_url': TEMPLATES_BASE_URL,
-        }
-
-        message = get_template('emails/reset_password.html').render(email_data)
-
-        msg = EmailMessage('Reset Password', body=message, to=[user.email,])
-        msg.content_subtype = 'html'
-        
-        try:
-            msg.send()
-        except:
-            pass
-        
-        return Response('reset_password_email_sent')
-    except Exception as e:
-        print(f'An error occurred: {str(e)}')
-        # You may want to log the error or handle it in a more appropriate way.
-        return Response('An error occurred while sending the password reset email.')
+    def has_permission(self, request, view):
+        return bool(request.user)
